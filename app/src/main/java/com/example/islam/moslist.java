380dcfdc7e9c2a95dcf152b1f21b51d2a7e085a6
Mohamed Abdelmaxod <mido99ahmed14@gmail.com>
2020-05-25 08:04:40 +0200

    full project isa

---
+++ app/src/main/java/com/example/islam/moslist.java
@@ -0,0 +1,9 @@
+package com.example.islam;
+
+public class moslist {
+    public String name;
+
+    public moslist(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/example/islam/progList.java
@@ -0,0 +1,9 @@
+package com.example.islam;
+
+public class progList {
+    public String name;
+
+    public progList(String name) {
+        this.name = name;
+    }
+}
